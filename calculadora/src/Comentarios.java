c078934e0958ce60ba322a109d1db202c67d53d8
Rodrigo <chocoliris@msn.com>
2024-08-12 17:02:01 -0300

    criação da classe comentario

---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Olá eu sou um comentário em uma única linha
+         
+        /* Olá
+          * Eu sou umcomentário
+          * que posso ser mais detalhado
+          * quando necessário
+          */
+          /**
+           * Estas duas estrelinhas
+           * é para identificar que você
+           * pretende elaborar um  comentário
+           * em forma de documentação
+           */
+          public void metodo(){
+
+          }
+
+    }
+
+}
