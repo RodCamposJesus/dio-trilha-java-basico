79600ad872224a2dc5758293e55604f64f15d08b
Rodrigo <chocoliris@msn.com>
2024-08-14 14:29:26 -0300

    Nova correção

--- calculadora/src/Calculadora.java
+++ calculadora/src/Calculadora.java
@@ -1,6 +1,6 @@
 /** 
 * @author Rodrigo
-* @Version 1.0
+* @version 1.0
 * @since 01/01/2022
 */
 
--- calculadora/src/Comentarios.java
+++ calculadora/src/Comentarios.java
@@ -1,8 +1,28 @@
 public class Comentarios {
     public static void main(String[] args) {
-
-    }
         
+    }
+        //Olá eu sou um comentário em uma única linha
+         
+        /* Olá
+          * Eu sou umcomentário
+          * que posso ser mais detalhado
+          * quando necessário
+          */
+          /**
+           * Estas duas estrelinhas
+           * é para identificar que você
+           * pretende elaborar um  comentário
+           * em forma de documentação
+           */
+    
+
+          /*
+           * Este método foi elaborado as pressas
+           * por isso  eu abreviei  o nome das variáveis
+           * mas olha,  ele tem a finalidade  somar ou multiplicar
+           * dois números
+           */
           public int somaMultiplica(int n, int x, String m){
             int r = 0; //r é igual ao resultado
             if(m == "M"){ //M = multiplicação
