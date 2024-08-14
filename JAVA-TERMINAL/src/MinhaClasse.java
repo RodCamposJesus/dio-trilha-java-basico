66f02c677268a97f139c666b11293ecc9dd9b373
Rodrigo <chocoliris@msn.com>
2024-08-14 14:49:23 -0300

    testando

--- JAVA-TERMINAL/src/MinhaClasse.java
+++ JAVA-TERMINAL/src/MinhaClasse.java
@@ -1,5 +1,5 @@
 public class MinhaClasse {
     public static void main(String[] args) throws Exception {
-        System.out.println("Oi, fui executada pelo terminal. ");
+        System.out.println("Oi turma , fui executada pelo terminal. ");
     }
 }
