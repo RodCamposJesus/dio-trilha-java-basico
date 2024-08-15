426de534ce15e241c831c5d96b98f5b4c1fa2d46
Rodrigo <chocoliris@msn.com>
2024-08-15 16:46:19 -0300

    classe Caixa Elereonico

---
+++ controle-fluxo/src/CaixaEletronic.java
@@ -0,0 +1,15 @@
+public class CaixaEletronic {
+    
+    public static void main(String[] args) throws Exception {
+        
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+
+            System.out.println(saldo);
+        }
+    }
+}
+
