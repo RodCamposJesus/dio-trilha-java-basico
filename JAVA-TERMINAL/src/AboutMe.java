e5433d88f273e7add36fc0918d22df186eb8e2d7
Rodrigo <chocoliris@msn.com>
2024-08-15 15:38:55 -0300

    Atualizando

--- JAVA-TERMINAL/src/AboutMe.java
+++ JAVA-TERMINAL/src/AboutMe.java
@@ -19,11 +19,7 @@ public class AboutMe {
         System.out.println("Digite sua altura ");
         double altura = scanner.nextDouble();
 
-        //String nome = args [0];
-        //String sobrenome = args [1];
-        //int idade = Integer.valueOf(args[2]);
-        //double altura = Double.valueOf(args[3]);
-        //imprimindo os dados obtidos pelo usuário
+    
         System.out.println("Olá me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " " + "anos. ");
         System.out.println("Minha altura é " + altura + " " + "cm. ");
