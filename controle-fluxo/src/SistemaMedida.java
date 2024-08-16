5d261a595a2f4e74c82d4dbd8a1a40463e9075b9
Rodrigo <chocoliris@msn.com>
2024-08-16 13:03:51 -0300

    classe SistemaMedida

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,20 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "M";
+
+        if (sigla == "P") 
+
+        System.out.println("Pequeno");
+        
+        else if(sigla == "M")
+        System.out.println("Pequeno");
+
+        else if(sigla == "G")
+        System.out.println("Grande");
+
+        else 
+        
+        System.out.println("Indefinido");
+              
+    }
+}
