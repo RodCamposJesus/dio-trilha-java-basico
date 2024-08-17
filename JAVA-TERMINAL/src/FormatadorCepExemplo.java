7160be3cecd8e6771a7da1783bef8f050876f97b
Rodrigo <chocoliris@msn.com>
2024-08-17 16:21:54 -0300

    teste

--- JAVA-TERMINAL/src/FormatadorCepExemplo.java
+++ JAVA-TERMINAL/src/FormatadorCepExemplo.java
@@ -1,6 +1,12 @@
 public class FormatadorCepExemplo {
     public static void main(String[] args) {
         
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            e.printStackTrace();
+        }
     }
 
     static String formatarCep(String cep) throws CepInvalidoException {
