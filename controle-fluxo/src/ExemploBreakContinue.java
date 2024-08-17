a943bc4f3e6bec0d4430756ed422077fefe2efdf
Rodrigo <chocoliris@msn.com>
2024-08-17 12:21:40 -0300

    teste

--- controle-fluxo/src/ExemploBreakContinue.java
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -2,10 +2,10 @@ public class ExemploBreakContinue {
     public static void main(String[] args) {
         
         for(int numero = 1; numero <= 5; numero ++){
-            if (numero == 3) {
+            if (numero == 3) 
                 break;
                 
-            }
+            System.out.println(numero);
         }
     }
 }
