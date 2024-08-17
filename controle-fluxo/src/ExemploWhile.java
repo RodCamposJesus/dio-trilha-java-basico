81616d99859e32a2f81853fc195ce32c686da0d7
Rodrigo <chocoliris@msn.com>
2024-08-17 13:03:33 -0300

    correção do código

--- controle-fluxo/src/ExemploWhile.java
+++ controle-fluxo/src/ExemploWhile.java
@@ -1,25 +1,26 @@
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ExemploWhile {
+
     public static void main(String[] args) {
-        
+
         double mesada = 50.0;
-        while (mesada > 0 ) {
-            Double valorDoce = valorAleatorio();
-            
+        while (mesada > 0) {
+            Double valorDoce = valorAleatorio(); // Call the method
+
             if (valorDoce > mesada) {
                 valorDoce = mesada;
-                
-                System.out.println("Doce do valor: " + valorDoce + "Adicionado no carrinho" );
-                mesada = mesada - valorDoce;
             }
-            System.out.println("Mesada: " + mesada);
-            System.out.println("Joãozinho  gastou toda a sua mesada em doces.");
+
+            System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+            mesada -= valorDoce; // Use subtraction operator directly
         }
 
-        private static double valorAleatorio(){
-            return ThreadLocalRandom.current().nextDouble(2, 8);
-        }
+        System.out.println("Mesada: " + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces.");
+    }
 
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 8);
     }
 }
